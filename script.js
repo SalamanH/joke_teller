587338f87c03392042a31e0599873733304a23aa
salamanH <salaman.hamidkohzad@gmail.com>
2020-10-01 14:10:40 -0400

    second commit: filtered jokes

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ async function getJokes()
 {
     
     let joke = '';
-    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark,Pun';
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
